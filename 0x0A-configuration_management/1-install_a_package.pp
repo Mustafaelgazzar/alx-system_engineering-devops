5a7912026f7b14f9df352166190723984d5aaddf
mustafaelgazzar <mustafaelgazzar@gmail.com>
2024-04-22 09:46:14 -0700

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/env puppet
+# install puppet-lint -v 2.1.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
